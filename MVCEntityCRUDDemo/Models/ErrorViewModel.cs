b02aa1be0ba406e2bc7d3c18e4e9e3acbc064ae4
KATTETI <brahma-swetha.katteti@capgemini.com>
2021-11-29 14:21:33 +0530

    First

---
+++ MVCEntityCRUDDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCEntityCRUDDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
